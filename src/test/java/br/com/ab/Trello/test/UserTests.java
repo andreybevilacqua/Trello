533b060217660c852cf282de7d3275cc3d855689
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2017-10-02 21:49:05 -0300

    Unit tests with Mockito.

--- src/test/java/br/com/ab/Trello/test/UserTests.java
+++ src/test/java/br/com/ab/Trello/test/UserTests.java
@@ -12,47 +12,67 @@ import br.com.ab.Trello.model.User;
 import junit.framework.Assert;
 
 public class UserTests {
-	
-	@Test(expected=ExceptionInInitializerError.class)
-	public void addUserFailureTest(){
-		
+
+	@Test(expected = ExceptionInInitializerError.class)
+	public void addUserFailureTest() {
+
 		UserDao userDao = new UserDao();
-		
+
 		User user = new User("Andrey", "Test");
 		user.setId(1);
-		
+
 		EntityManager entityManager = new JPAUtil().getEntityManager();
-		
+
 		userDao.addUser(user, entityManager);
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@Test
-	public void addUserSucessfullyTest(){
-		
+	public void addUserSucessfullyTest() {
+
 		UserDao userDao = new UserDao();
 		userDao.setEntityManager(Mockito.mock(EntityManager.class));
-		
-		//ArgumentCaptor<SuccessfulPaymentEvent> argumentCaptor = ArgumentCaptor.forClass(SuccessfulPaymentEvent.class);
+
+		// ArgumentCaptor<SuccessfulPaymentEvent> argumentCaptor =
+		// ArgumentCaptor.forClass(SuccessfulPaymentEvent.class);
 		User user = new User("Andrey", "Test");
 		userDao.addUser(user);
-		
+
 		Mockito.verify(userDao.getEntityManager()).persist(user);
-		
-		int user_Id = 14; 
-		
+
+		int user_Id = 14;
+
 		Assert.assertEquals(user.getId().intValue(), user_Id);
 	}
-	
+
 	@Test
-	public void findUserByName(){
+	public void testDBConnection(){
 		
-		User user;
-		UserDao userDao = new UserDao();
+		EntityManager entityManager = Mockito.mock(EntityManager.class);
+		String usernameTest = "Andrey";
+		String databaseReturn = (String)entityManager.createQuery("SELECT username FROM user WHERE user_id = 1").getSingleResult();
 		
-		user = userDao.findUserByLogin("Amanda");
-		
-		assertEquals("Amanda", user.getLogin());
-		
+		assertEquals(usernameTest, databaseReturn);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
