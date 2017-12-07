73488b73ef8a154b7d11dc875a28eee24403c301
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2017-12-07 20:10:05 +0100

    Working on UserDao unit tests with Mockito.

--- src/main/java/br/com/ab/Trello/dao/UserDao.java
+++ src/main/java/br/com/ab/Trello/dao/UserDao.java
@@ -31,10 +31,6 @@ public class UserDao {
 		this.entityManager.persist(user);
 	}
 	
-	public void addUser(User user, EntityManager entityManager){
-		entityManager.persist(user);
-	}
-	
 	public User findUserById(Integer userId){
 		User user = this.entityManager.find(User.class, userId);
 		return user;
--- src/test/java/br/com/ab/Trello/test/UserTests.java
+++ src/test/java/br/com/ab/Trello/test/UserTests.java
@@ -1,49 +1,47 @@
 package br.com.ab.Trello.test;
 
-import br.com.ab.Trello.dao.UserDao;
-import br.com.ab.Trello.model.User;
-import junit.framework.Assert;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
-import javax.persistence.EntityManager;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import br.com.ab.Trello.dao.UserDao;
+import br.com.ab.Trello.model.User;
+import junit.framework.Assert;
 
 public class UserTests {
 
 	private UserDao userDao;
+	private EntityManager entityManager;
 
 	@Before
 	public void setup() {
+
 		userDao = new UserDao();
-	}
+		entityManager = Mockito.mock(EntityManager.class);
 
-	@Test(expected = ExceptionInInitializerError.class)
-	public void addUserFailureTest() {
-
-		User user = new User("Andrey", "Test");
-		user.setId(1);
-
-		EntityManager entityManager = new JPAUtil().getEntityManager();
-
-		userDao.addUser(user, entityManager);
+		userDao.setEntityManager(entityManager);
 	}
 
 	@SuppressWarnings("deprecation")
-	@Test
+	@Test // Eu mantive esse teste igual como o Ingo fez, para usar como
+			// exemplo.
 	public void addUserSucessfullyTest() {
 
-		// Objeto EntityManager vai ser simulado pelo Mockito, na hora que executar o userDao.addUser;
+		// Objeto EntityManager vai ser simulado pelo Mockito, na hora que
+		// executar o userDao.addUser;
 		EntityManager entityManager = Mockito.mock(EntityManager.class);
 
 		Mockito.doAnswer(new MockUserAnswer(1, "Andrey", "Test")).when(entityManager).persist(Mockito.any(User.class));
 
-		// Precisa de get/set entityManager na userDao, para que tu possa criar o objeto entityManager com Mockito.mock;
+		// Precisa de get/set entityManager na userDao, para que tu possa criar
+		// o objeto entityManager com Mockito.mock;
 		userDao.setEntityManager(entityManager);
 
 		User user = new User("Andrey", "Test");
@@ -55,27 +53,94 @@ public class UserTests {
 		Assert.assertNotSame(user.getId(), 2);
 	}
 
-	@SuppressWarnings({ "unused", "deprecation" })
+	@SuppressWarnings("deprecation")
 	@Test
-	public void findUserByIdSucessfullyTest() {
+	public void findUserByIdSucessfullyTest() throws InstantiationException, IllegalAccessException {
 
-		EntityManager entityManager = Mockito.mock(EntityManager.class);
-
-		Mockito.doAnswer(new MockUserAnswer(1, "Andrey", "Test")).when(entityManager).persist(Mockito.any(User.class));
-		
-		userDao.setEntityManager(entityManager);
-		
-		// Creating and adding user, so we can test if findUserById will work.
 		User user = new User("Andrey", "Test");
-		userDao.addUser(user);
-		
-		Mockito.verify(userDao.getEntityManager()).persist(user);
-		
-		User userTest = userDao.findUserById(1);
-		
-		Assert.assertEquals("Andrey", userTest.getLogin());
-		Assert.assertEquals("Test", userTest.getPassword());
+		user.setId(1);
 
+		// Quando o entityManager realizar o find, buscando pelo User.class, e
+		// passando user.getId() como parametro,
+		// então retorna o objeto user.
+		Mockito.when(entityManager.find(User.class, user.getId())).thenReturn(user);
+
+		userDao.setEntityManager(entityManager); // Nao precisa mais, pois esta
+													// no @Before.
+
+		User userTest = userDao.findUserById(user.getId());
+
+		Assert.assertSame(user, userTest);
+
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void findUserByUnknownIdTest() {
+
+		int id = -1;
+		Mockito.when(entityManager.find(User.class, id)).thenReturn(null);
+
+		User userTest = userDao.findUserById(id);
+
+		Mockito.verify(entityManager).find(User.class, id);
+
+		Assert.assertNull(userTest);
+		Assert.assertSame(userTest, null);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void findUserByLogin() {
+
+		String login = "Andrey";
+		Mockito.when(entityManager.find(User.class, login)).thenReturn(new User("Andrey", "Test"));
+
+		User userTest = userDao.findUserByLogin(login);
+
+		Assert.assertEquals(userTest.getLogin(), login);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void findAllUsers() {
+
+		List<User> listOfUsers = new ArrayList<User>();
+
+		Mockito.doAnswer(new MockAllUsersAnswer()).when(entityManager).createQuery("SELECT u FROM USER u", User.class)
+				.getResultList();
+
+		listOfUsers = userDao.findAllUser();
+		
+		Mockito.verify(userDao.getEntityManager()).createQuery("SELECT u FROM USER u", User.class);
+		//Mockito.verify(userDao.getEntityManager()).persist(user);
+
+		Assert.assertEquals(3, listOfUsers.size());
+
+	}
+
+	private class MockAllUsersAnswer implements Answer<Void> {
+
+		private List<User> listOfUsers;
+
+		private MockAllUsersAnswer() {
+
+			listOfUsers = new ArrayList<User>();
+
+			listOfUsers.add(new User("Andrey", "Teste"));
+			listOfUsers.add(new User("Morena", "Teste"));
+			listOfUsers.add(new User("Vinicius", "Teste"));
+			listOfUsers.add(new User("Georgia", "Teste"));
+		}
+
+		@Override
+		public Void answer(InvocationOnMock invocation) throws Throwable {
+
+			List<User> listOfUserInvocation = invocation.getArgument(0);
+			listOfUserInvocation.addAll(listOfUsers);
+
+			return null;
+		}
 	}
 
 	private class MockUserAnswer implements Answer<Void> {
